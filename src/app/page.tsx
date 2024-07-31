aedafcb6a2ae1d9b5a5a3c7a13ff11fcc95e1dd4
AnilBurcu <anlbrc95@gmail.com>
2024-07-31 12:41:02 +0300

    Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,9 @@ export default function Home() {
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 Your image on a{" "}
-                <span className="bg-orange-900 px-2 text-white">Custom</span>
+                <span className="bg-orange-500 rounded-xl px-2 text-white">
+                  Custom
+                </span>
                 Phone Case
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
